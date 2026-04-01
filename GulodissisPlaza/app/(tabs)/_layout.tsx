1468a7f9040537c00ab28d918d99311e1e73eea6
Alanzouka <semata654321@gmail.com>
2026-04-01 08:59:17 -0300

    adicionei imagem

--- GulodissisPlaza/app/(tabs)/_layout.tsx
+++ GulodissisPlaza/app/(tabs)/_layout.tsx
@@ -19,14 +19,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Login',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Cardápio',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
